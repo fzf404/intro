613e76499c2422eff32de0db9d6660d4ca42582b
你们的小f <nmdfzf404@163.com>
2021-10-27 22:23:41 +0800

    ⚡️ 修复错误处理

--- web/assets/js/intro.js
+++ web/assets/js/intro.js
@@ -1,7 +1,7 @@
 /*
  * @Author: fzf404
  * @Date: 2021-10-10 21:34:19
- * @LastEditTime: 2021-10-27 18:20:21
+ * @LastEditTime: 2021-10-27 22:21:34
  * @Description: 自我介绍详情
  */
 
@@ -11,12 +11,19 @@ const url_params = new URLSearchParams(window.location.search)
 // 判断是否传入id参数
 if (!url_params.has('id')) {
   // 向错误处理传入错误信息
-  handle_code({"code":404,"msg":"传入的学号不存在!"})
+  handle_code({ "code": 404, "msg": "传入的学号不存在!" })
 }
 
 // 发送请求
-$.getJSON(`${base_url}/intro?id=${url_params.get('id')}`, function (json) {
-  
+$.get(`${base_url}/intro?id=${url_params.get('id')}`, function (json) {
+
+  // 值验证
+  if (typeof (json) == 'string') {
+    json = JSON.parse(json)
+  }
+
+  handle_code(json)
+
   $('#name').text(json.data.name)
   $("#intro").text(json.data.intro)
   $("#about").text(json.data.about)
@@ -30,7 +37,7 @@ $.getJSON(`${base_url}/intro?id=${url_params.get('id')}`, function (json) {
   // 展示页面
   display()
 
-}).error(
+}).fail(
   // 错误处理
   () => handle_fail()
 )
--- web/assets/js/total.js
+++ web/assets/js/total.js
@@ -1,12 +1,17 @@
 /*
  * @Author: fzf404
  * @Date: 2021-10-11 20:04:18
- * @LastEditTime: 2021-10-27 18:19:58
+ * @LastEditTime: 2021-10-27 22:20:53
  * @Description: 自我介绍列表
  */
 
-$.getJSON(`${base_url}/total`, function (json) {
+$.get(`${base_url}/total`, function (json) {
 
+  // 值验证
+  if (typeof (json) == 'string') {
+    json = JSON.parse(json)
+  }
+  
   // 判断code
   handle_code(json)
 
@@ -18,7 +23,7 @@ $.getJSON(`${base_url}/total`, function (json) {
   // 关闭加载中，展示内容
   display()
 
-}).error(
+}).fail(
   // 错误处理
   () => handle_fail()
 )
